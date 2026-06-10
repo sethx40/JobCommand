9d7191a1e4ef32f57a43cf6d401461accd4a378d
sethx40 <sethdodson47@gmail.com>
2026-06-09 19:43:41 -0500

    Add files via upload

--- supabase-config.js
+++ supabase-config.js
@@ -1,3 +1,3 @@
 export const SUPABASE_URL = "https://opuazyjejcibutbglfc.supabase.co";
-export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9wdWF6eXpqZWpjaWJ1dGJnbGZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEwNDA3NzYsImV4cCI6MjA5NjYxNjc3Nn0.3FzEcMiN6NEm7ajb6kjm5ZU1nAttDAE_tM8abrotVRc";
+export const SUPABASE_ANON_KEY = "https://opuazyzjejcibutbglfc.supabase.co";
 export const DEFAULT_COMPANY_NAME = "JobCommand Beta";
